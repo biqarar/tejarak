335de8abefb883c46bfbb15318133cb574a80707
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-05-16 17:52:04 +0430

    add config

--- config.php
+++ config.php
@@ -7,14 +7,12 @@
 // ** MySQL settings - You can get this info from your web host ** //
  /** The name of the database */
 if(!defined('db_name'))
- define("db_name", '_db_');
+ define("db_name", 'tejarak');
 
  /** MySQL database username */
-if(!defined('db_user'))
- define("db_user", '_user_');
+ define("db_user", 'ermile');
 
  /** MySQL database password */
-if(!defined('db_pass'))
- define("db_pass", '_pass_');
+ define("db_pass", 'Ermile@#$1233');
 
 ?>
