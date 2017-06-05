6d12e74cb884af6c36964d6cea5e8151de5c6d4a
Reza <rm.biqarar@gmail.com>
2017-06-05 12:23:03 +0430

    add option page

---
+++ content_admin/option/model.php
@@ -0,0 +1,10 @@
+<?php
+namespace content_admin\option;
+use \lib\debug;
+use \lib\utility;
+
+class model extends \content_admin\main\model
+{
+
+}
+?>
---
+++ content_admin/option/view.php
@@ -0,0 +1,8 @@
+<?php
+namespace content_admin\option;
+
+class view extends \content_admin\main\view
+{
+
+}
+?>
