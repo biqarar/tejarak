67c4eb23857c2bee81e93e347a980e0232540c49
Reza Mohiti <rm.biqarar@gmail.com>
2017-06-13 15:40:18 +0430

    add ban page

---
+++ content_enter/ban/controller.php
@@ -0,0 +1,14 @@
+<?php
+namespace content_enter\ban;
+
+
+class controller extends \content_enter\main\controller
+{
+	public function _route()
+	{
+		// if the user is login redirect to base
+		parent::if_login_not_route();
+		
+	}
+}
+?>
