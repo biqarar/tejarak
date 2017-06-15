759d4c7a5c064a5b14c73d57a560e7ccd52a10e1
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-06-15 18:58:52 +0430

    fix sessions

--- content_enter/sessions/view.php
+++ content_enter/sessions/view.php
@@ -11,7 +11,44 @@ class view extends \content_enter\main\view
 	 */
 	public function view_sessions($_args)
 	{
-		$this->data->sessions_list = $this->model()->sessions_list();
+		$mySessions    = $this->model()->sessions_list();
+		$mySessionData = [];
+		foreach ($mySessions as $key => $row)
+		{
+			@$mySessionData[$key]['id']         = $row['id'];
+			@$mySessionData[$key]['ip']         = long2ip($row['ip']);
+			@$mySessionData[$key]['last']       = $row['last_seen'];
+			@$mySessionData[$key]['browser']    = ucfirst($row['agent_name']);
+			@$mySessionData[$key]['browserVer'] = $row['agent_version'];
+			@$mySessionData[$key]['os']         = $row['agent_os'];
+			@$mySessionData[$key]['osVer']      = $row['agent_osnum'];
+
+			if(isset($row['agent_os']))
+			{
+				switch ($row['agent_os'])
+				{
+					case 'nt':
+						$mySessionData[$key]['os'] = 'Windows';
+						break;
+
+					case 'lin':
+						$mySessionData[$key]['os'] = 'Linux';
+						break;
+
+					default:
+						break;
+				}
+			}
+		}
+
+		$this->data->sessions_list = $mySessionData;
+		// var_dump($this->data->sessions_list);
+		// $myAgent = $this->data->sessions_list[0]['agent_agent'];
+		// $myBrowser = \lib\utility\browserDetection::browser_detection('full_assoc', '', $myAgent);
+
+
+
+
 
 		$this->data->page['title']   = T_('Active sessions');
 		$this->data->page['desc']    = $this->data->page['title'];
