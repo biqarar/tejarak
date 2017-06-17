780a50f57795897c310c3dc2421f84616f5263c7
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-06-17 14:45:06 +0430

    fix options

--- option.php
+++ option.php
@@ -89,18 +89,18 @@ self::$social['facebook']['client_token']     = 'df0047eb1af1e2acba2a3645bcb4f47
 /**
 * GOOGLE
 */
-self::$social['google']['status']                      = true;
-self::$social['google']['clinet_id']                   = '395232553225-140n5eq7ha34thagrbcnns125ocmqk96.apps.googleusercontent.com';
-self::$social['google']['project_id']                  = 'ermile-tejarak';
-self::$social['google']['auth_uri']                    = 'https://accounts.google.com/o/oauth2/auth';
-self::$social['google']['token_uri']                   = 'https://accounts.google.com/o/oauth2/token';
-self::$social['google']['auth_provider_x509_cert_url'] = 'https://www.googleapis.com/oauth2/v1/certs';
-self::$social['google']['client_secret']               = 'h3q_yNJFqbqO5SV-PK2cXMmc';
+// self::$social['google']['status']                      = true;
+// self::$social['google']['client_id']                   = '395232553225-140n5eq7ha34thagrbcnns125ocmqk96.apps.googleusercontent.com';
+// self::$social['google']['project_id']                  = 'ermile-tejarak';
+// self::$social['google']['auth_uri']                    = 'https://accounts.google.com/o/oauth2/auth';
+// self::$social['google']['token_uri']                   = 'https://accounts.google.com/o/oauth2/token';
+// self::$social['google']['auth_provider_x509_cert_url'] = 'https://www.googleapis.com/oauth2/v1/certs';
+// self::$social['google']['client_secret']               = 'h3q_yNJFqbqO5SV-PK2cXMmc';
 
 
 
 self::$social['google']['status']                      = true;
-self::$social['google']['clinet_id']                   = '395232553225-5filcn07d2rdjl2fld57mf8e50ac146j.apps.googleusercontent.com';
+self::$social['google']['client_id']                   = '395232553225-5filcn07d2rdjl2fld57mf8e50ac146j.apps.googleusercontent.com';
 self::$social['google']['project_id']                  = 'ermile-tejarak';
 self::$social['google']['auth_uri']                    = 'https://accounts.google.com/o/oauth2/auth';
 self::$social['google']['token_uri']                   = 'https://accounts.google.com/o/oauth2/token';
