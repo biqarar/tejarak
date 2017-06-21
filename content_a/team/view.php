606b005729e675c7cdf2c142145254591bd2238f
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-06-21 17:49:50 +0430

    fix title and desc of page

--- content_a/team/view.php
+++ content_a/team/view.php
@@ -9,7 +9,8 @@ class view extends \content_a\main\view
 	 */
 	public function view_add()
 	{
-
+		$this->data->page['title'] = 'Add new team';
+		$this->data->page['desc'] = 'Add new team';
 	}
 
 	/**
@@ -25,6 +26,12 @@ class view extends \content_a\main\view
 			$this->data->team = $this->model()->edit($name);
 		}
 		$this->data->edit_mode = true;
+
+		if(isset($this->data->team['name']))
+		{
+			$this->data->page['title'] = 'Edit team';
+			$this->data->page['desc'] = 'Edit team'. $this->data->team['name'];
+		}
 	}
 }
 ?>
