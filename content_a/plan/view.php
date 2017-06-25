c3eb2cf55fbed2890707b659ed5d262bff293c81
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-06-25 19:11:33 +0430

    add simple design to plans page

--- content_a/plan/view.php
+++ content_a/plan/view.php
@@ -13,6 +13,8 @@ class view extends \content_a\main\view
 	public function view_plan($_args)
 	{
 		$this->data->current_plan = $this->model()->plan();
+		$this->data->page['title'] = T_('Change Plan of :name', ['name'=>'myTeam']);
+		$this->data->page['desc']  = $this->data->page['title'];
 	}
 
 }
