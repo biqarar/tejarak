7ebbb53f420e3e1ed8335c3673404c4b48c0136e
Reza <rm.biqarar@gmail.com>
2017-06-26 18:03:14 +0430

    change hours date to varchar

---
+++ includes/cls/database/upgrade/(db_name)/v.1.2.3_change_shmasi_date_type.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `hours` CHANGE `shamsi_date` `shamsi_date` VARCHAR(10) NOT NULL;
+ALTER TABLE `hours` CHANGE `endshamsi_date` `endshamsi_date` VARCHAR(10) NULL;
+ALTER TABLE `hourlogs` CHANGE `shamsi_date` `shamsi_date` VARCHAR(10) NOT NULL;
