37dad14062cd68277503fc0d34125aad5d0a2830
Reza <rm.biqarar@gmail.com>
2017-06-26 16:27:54 +0430

    redirect gateway to hours page

--- content_a/home/controller.php
+++ content_a/home/controller.php
@@ -24,6 +24,24 @@ class controller extends \content_a\main\controller
 				return;
 			}
 		}
+
+		// check if the user is gateway redirect to hours page
+		if(!$this->login('mobile') && $this->login('parent') && $this->login('username') && $this->login('pass'))
+		{
+			$check_is_gateway = \lib\db\userteams::get(['user_id' => $this->login('id'), 'rule' => 'gateway', 'limit'=> 1]);
+			if(isset($check_is_gateway['team_id']))
+			{
+				$shortname = \lib\db\teams::get_by_id($check_is_gateway['team_id']);
+				if(isset($shortname['shortname']))
+				{
+
+					$this->redirector($this->view()->url->base. '/'. $shortname['shortname'])->redirect();
+					return;
+				}
+			}
+
+		}
+
 		// list of all team the user is them
 		$this->get(false, 'dashboard')->ALL();
 
