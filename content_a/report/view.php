77ee41f821189838c434b20b611f30941af177de
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-06-26 13:28:06 +0430

    fix report and last report

--- content_a/report/view.php
+++ content_a/report/view.php
@@ -6,6 +6,9 @@ class view extends \content_a\main\view
 	public function config()
 	{
 		parent::config();
+
+		$this->data->page['title'] = T_('Reports');
+		$this->data->page['desc']  = $this->data->page['title'];
 	}
 }
 ?>
