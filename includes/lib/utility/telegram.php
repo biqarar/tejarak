04d5ea2b927f55b417b19577f6541c13c4f21dc1
Reza <rm.biqarar@gmail.com>
2017-06-30 17:30:38 +0430

    telegram send Message bug fix

--- includes/lib/utility/telegram.php
+++ includes/lib/utility/telegram.php
@@ -16,22 +16,44 @@ class telegram
 //     "chat_id" : 33263188
 //   }'
 
-	public static function config()
+	public static function tg_curl($_args = [])
 	{
-		$url = 'http://178.62.218.8:8081/get_request_hook';
+		$default_args =
+		[
+			'url'            => 'http://178.62.218.8:8081/get_request_hook',
+			'app_name'       => 'tejarak',
+			'request_method' => 'sendMessage',
+			'telegram_id'    => null,
+			'request_id'     => 0,
+			'text'           => null,
+			'file'           => null,
+		];
+
+		if(is_array($_args))
+		{
+			$_args = array_merge($default_args, $_args);
+		}
+
+		$url = $_args['url'];
+
+		if(!$_args['url'] || !$_args['request_method'] || !$_args['telegram_id'] || !$_args['app_name'])
+		{
+			return false;
+		}
+
 		$headers =
 		[
-			'app-name: tejarak',
-			'content-type: application/json',
-			'request-id: 0',
-			'request-method: sendMessage',
-			'telegram-id: 33263188',
+			"app-name: $_args[app_name]",
+			"content-type: application/json",
+			"request-id: $_args[request_id]",
+			"request-method: $_args[request_method]",
+			"telegram-id: $_args[telegram_id]",
 		];
 
 		$content =
 		[
-			'text'    => 'سلام رضا خوبی',
-			'chat_id' => 33263188,
+			'text'    => $_args['text'],
+			'chat_id' => $_args['telegram_id'],
 		];
 
 		$content = json_encode($content, JSON_UNESCAPED_UNICODE);
@@ -52,17 +74,36 @@ class telegram
 
 			$response = curl_exec($handle);
 			$mycode   = curl_getinfo($handle, CURLINFO_HTTP_CODE);
-			// check mycode in special situation, if has default code with status handle it
+
 			curl_close ($handle);
-			//=====================================================================================//
-			// for debug you can uncomment below line to see the result get from server
-			// var_dump($response);
-			var_dump($response);exit();
+
+			\lib\db\logs::set("telegram:curl:$_args[request_method]", null, ['meta' => ['response' => $response, 'http_code' => $mycode]]);
 		}
 		else
 		{
+			\lib\db\logs::set('telegram:curl:not:install', null, ['meta' =>[]]);
 			\lib\debug::warn(T_("Please install curl on your system"));
 		}
+	}
+
+
+	/**
+	 * Sends a message via telegram
+	 *
+	 * @param      <type>  $_chat_id  The chat identifier
+	 * @param      <type>  $_text     The text
+	 *
+	 * @return     <type>  ( description_of_the_return_value )
+	 */
+	public static function sendMessage($_chat_id, $_text)
+	{
+		$args =
+		[
+			'request_method' => 'sendMessage',
+			'telegram_id'    => $_chat_id,
+			'text'           => $_text,
+		];
+		return self::tg_curl($args);
 	}
 }
 ?>
