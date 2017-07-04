604e90db16b2cb828696a60a20a1cb0d26426b9a
Reza <rm.biqarar@gmail.com>
2017-07-04 12:17:56 +0430

    show team name in edit plan

--- content_a/plan/view.php
+++ content_a/plan/view.php
@@ -13,7 +13,20 @@ class view extends \content_a\main\view
 	public function view_plan($_args)
 	{
 		$this->data->current_plan = $this->model()->plan();
-		$this->data->page['title'] = T_('Change Plan of :name', ['name'=>'myTeam']);
+
+		$myTeam = 'myTeam';
+
+		if(isset($this->data->current_plan['team_id']))
+		{
+			$team_code = \lib\utility\shortURL::encode($this->data->current_plan['team_id']);
+			$current_team = $this->model()->getTeamDetail($team_code);
+
+			if(isset($current_team['name']))
+			{
+				$myTeam = $current_team['name'];
+			}
+		}
+		$this->data->page['title'] = T_('Change Plan of :name', ['name'=>$myTeam]);
 		$this->data->page['desc']  = $this->data->page['title'];
 	}
 
