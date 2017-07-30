7f6bee09d3665203201414df47d0a3395c5fca4c
Reza <rm.biqarar@gmail.com>
2017-07-30 11:36:52 +0430

    check date in add hour edit request

--- content_api/v1/houredit/tools/add.php
+++ content_api/v1/houredit/tools/add.php
@@ -58,70 +58,70 @@ trait add
 		$id = utility\shortURL::decode($id);
 		if(utility::request('id') && !$id)
 		{
-			logs::set('api:houredit:id:not:set', null, $log_meta);
+			logs::set('api:houredit:id:not:set', $this->user_id, $log_meta);
 			debug::error(T_("Id not set"), 'id', 'arguments');
 			return false;
 		}
 
 		if(!$start_date)
 		{
-			logs::set('api:houredit:start_date:not:set', null, $log_meta);
+			logs::set('api:houredit:start_date:not:set', $this->user_id, $log_meta);
 			debug::error(T_("Start date not set"), 'start_date', 'arguments');
 			return false;
 		}
 
 		if(\DateTime::createFromFormat('Y-m-d', $start_date) === false)
 		{
-		 	logs::set('api:houredit:start_date:invalid', null, $log_meta);
+		 	logs::set('api:houredit:start_date:invalid', $this->user_id, $log_meta);
 			debug::error(T_("Invalid start date"), 'start_date', 'arguments');
 			return false;
 		}
 
 		if(!$start_time)
 		{
-			logs::set('api:houredit:start_time:not:set', null, $log_meta);
+			logs::set('api:houredit:start_time:not:set', $this->user_id, $log_meta);
 			debug::error(T_("Start time not set"), 'start_time', 'arguments');
 			return false;
 		}
 
 		if(\DateTime::createFromFormat('H:i', $start_time) === false && \DateTime::createFromFormat('H:i:s', $start_time) === false)
 		{
-		 	logs::set('api:houredit:start_time:invalid', null, $log_meta);
+		 	logs::set('api:houredit:start_time:invalid', $this->user_id, $log_meta);
 			debug::error(T_("Invalid start time"), 'start_time', 'arguments');
 			return false;
 		}
 
 		if(!$end_date)
 		{
-			logs::set('api:houredit:end_date:not:set', null, $log_meta);
+			logs::set('api:houredit:end_date:not:set', $this->user_id, $log_meta);
 			debug::error(T_("end date not set"), 'end_date', 'arguments');
 			return false;
 		}
 
 		if(\DateTime::createFromFormat('Y-m-d', $end_date) === false)
 		{
-		 	logs::set('api:houredit:end_date:invalid', null, $log_meta);
+		 	logs::set('api:houredit:end_date:invalid', $this->user_id, $log_meta);
 			debug::error(T_("Invalid end date"), 'end_date', 'arguments');
 			return false;
 		}
 
 		if(!$end_time)
 		{
-			logs::set('api:houredit:end_time:not:set', null, $log_meta);
+			logs::set('api:houredit:end_time:not:set', $this->user_id, $log_meta);
 			debug::error(T_("end time not set"), 'end_time', 'arguments');
 			return false;
 		}
 
 		if(\DateTime::createFromFormat('H:i', $end_time) === false && \DateTime::createFromFormat('H:i:s', $end_time) === false)
 		{
-		 	logs::set('api:houredit:end_time:invalid', null, $log_meta);
+		 	logs::set('api:houredit:end_time:invalid', $this->user_id, $log_meta);
 			debug::error(T_("Invalid end time"), 'end_time', 'arguments');
 			return false;
 		}
 
 		if($desc && mb_strlen($desc) > 500)
 		{
-			logs::set('api:houredit:desc:max:length', null, $log_meta);
+			logs::set('api:houredit:desc:max:length', $this->user_id, $log_meta);
 			debug::error(T_("You must be set less than 500 character in description field"), 'desc', 'arguments');
 			return false;
 		}
@@ -130,9 +130,70 @@ trait add
 		$team_id = utility\shortURL::decode($team_id);
 		if(!$team_id)
 		{
-			logs::set('api:houredit:team:id:not:set', null, $log_meta);
+			logs::set('api:houredit:team:id:not:set', $this->user_id, $log_meta);
 			debug::error(T_("Team id not set"), 'team', 'arguments');
 			return false;
+		}
+		// get userteam detail
+		$userteam_detail = \lib\db\teams::access_team_id($team_id, $this->user_id, ['action' => 'in_team']);
+
+		if(!isset($userteam_detail['userteam_createdate']))
+		{
+			logs::set('api:houredit:userteam:id:not:found', $this->user_id, $log_meta);
+			debug::error(T_("This user is not in this team"), 'team', 'arguments');
+			return false;
+		}
+		// get the first date this user added to this team
+		$date_added_to_team = $userteam_detail['userteam_createdate'];
+		// set gregorian date
+		$start_date_gregorian = $start_date;
+		// if start date is jalali convert to gregorian
+		if(utility\jdate::is_jalali($start_date))
+		{
+			$start_date_gregorian = utility\jdate::to_gregorian($start_date, "Y-m-d");
+		}
+
+		// check start date > date added user in team
+		if(strtotime($start_date_gregorian) < strtotime($date_added_to_team))
+		{
+			logs::set('api:houredit:start:date:less:than:added:to:team', $this->user_id, $log_meta);
+			debug::error(T_("On this time you are not in this team!"), 'start_date', 'arguments');
+			return false;
+		}
+
+		// set gregorian date
+		$end_date_gregorian = $end_date;
+		// if end date is jalali convert to gregorian
+		if(utility\jdate::is_jalali($end_date))
+		{
+			$end_date_gregorian = utility\jdate::to_gregorian($end_date, "Y-m-d");
+		}
+
+		// check end date > date added user in team
+		if(strtotime($end_date_gregorian) > time())
+		{
+			logs::set('api:houredit:end:date:larger:than:now', $this->user_id, $log_meta);
+			debug::error(T_("End date can not larger than date now!"), 'end_date', 'arguments');
+			return false;
+		}
+
+		// check end date > start date
+		if(strtotime($end_date_gregorian) < strtotime($start_date_gregorian))
+		{
+			logs::set('api:houredit:end:date:larger:start:date', $this->user_id, $log_meta);
+			debug::error(T_("End date can not larger than start date"), 'end_date', 'arguments');
+			return false;
+		}
+
+		// check start and end time in one date
+		if(strtotime($end_date_gregorian) === strtotime($start_date_gregorian))
+		{
+			if(strtotime("$end_date_gregorian $end_time") < strtotime("$start_date_gregorian $start_time"))
+			{
+				logs::set('api:houredit:end:time:larger:start:time:date:is:equal', $this->user_id, $log_meta);
+				debug::error(T_("Can not set end time larger than start time in one date"), 'end_time', 'arguments');
+				return false;
+			}
 		}
 
 		// the request have hour id
@@ -140,7 +201,7 @@ trait add
 		{
 			// load hour data
 			$hour_detail = \lib\db\hours::access_hours_id($id, $this->user_id, ['action' => 'view']);
-
+			// var_dump($hour_detail);exit();
 			// check the hour exist
 			if(isset($hour_detail['id']))
 			{
@@ -148,7 +209,7 @@ trait add
 			}
 			else
 			{
-				logs::set('api:houredit:hour:notfound:invalid', null, $log_meta);
+				logs::set('api:houredit:hour:notfound:invalid', $this->user_id, $log_meta);
 				debug::error(T_("Can not access to set time of this hour"), 'hour', 'permission');
 				return false;
 			}
@@ -183,7 +244,7 @@ trait add
 
 			if($check_exist)
 			{
-				logs::set('api:houredit:duplicate:start:date:hour_id:is:null', null, $log_meta);
+				logs::set('api:houredit:duplicate:start:date:hour_id:is:null', $this->user_id, $log_meta);
 				debug::error(T_("Duplicate request of this start time"), 'id', 'arguments');
 				return false;
 			}
@@ -210,7 +271,7 @@ trait add
 		}
 		else
 		{
-			logs::set('api:houredit:method:error', null, $log_meta);
+			logs::set('api:houredit:method:error', $this->user_id, $log_meta);
 			debug::error(T_("Syste error"), 'system', 'error');
 			return false;
 		}
