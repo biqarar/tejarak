5c97f4cede3734729a54a268fe246e38354d714b
Reza <rm.biqarar@gmail.com>
2017-08-02 14:56:58 +0430

    Transfer from saloos to ermile

--- public_html/index.php
+++ public_html/index.php
@@ -3,13 +3,13 @@
  @ In the name Of Allah
 **/
 
-// if Saloos exist, require it else show related error message
-if ( file_exists( '../../saloos/autoload.php') )
+// if Ermile exist, require it else show related error message
+if ( file_exists( '../../ermile/autoload.php') )
 {
-	require_once( '../../saloos/autoload.php');
+	require_once( '../../ermile/autoload.php');
 }
 else
 {   // A config file doesn't exist
-	exit("<p>We can't find <b>Saloos</b>! Please contact administrator!</p>");
+	exit("<p>We can't find <b>Ermile</b>! Please contact administrator!</p>");
 }
 ?>
