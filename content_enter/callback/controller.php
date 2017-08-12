dcfec35c06e4d5ad2affb5a1c8eec4baafe38d27
Reza <rm.biqarar@gmail.com>
2017-08-12 14:00:28 +0430

    change uid in kavenegar sms

--- content_enter/callback/controller.php
+++ content_enter/callback/controller.php
@@ -9,9 +9,9 @@ class controller extends \content_enter\main\controller
 	{
 		// 10002000200251
 
-		\lib\utility\telegram::sendMessage(33263188, "#SMS_CALLBACK\n".  json_encode($_REQUEST, JSON_UNESCAPED_UNICODE));
+		\lib\utility\telegram::sendMessage(33263188, "#SMS_CALLBACK\n".  json_encode(['get' => $_GET, 'post' => $_POST], JSON_UNESCAPED_UNICODE));
 
-		if(!utility::get('service') || utility::get('uid') != '201700001')
+		if(!utility::get('service') || utility::get('uid') != '201708111')
 		{
 			\lib\error::page(T_("Invalid url"));
 		}
