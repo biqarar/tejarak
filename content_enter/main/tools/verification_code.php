c9d34f7e09d0daa8fe7359e2bcf17fd6f688b0fa
Reza <rm.biqarar@gmail.com>
2017-08-13 13:26:04 +0430

    enter verify by sendsms bug fix

--- content_enter/main/tools/verification_code.php
+++ content_enter/main/tools/verification_code.php
@@ -142,6 +142,15 @@ trait verification_code
 
 	public static function check_code($_module)
 	{
+		$log_meta =
+		[
+			'meta' =>
+			[
+				'session' => $_SESSION,
+				'post'    => utility::post(),
+			]
+		];
+
 		if(!self::check_input_current_mobile())
 		{
 			debug::error(T_("Dont!"));
@@ -160,7 +169,86 @@ trait verification_code
 			return false;
 		}
 
-		if(intval(utility::post('code')) === intval(self::get_enter_session('verification_code')))
+		$code_is_okay = false;
+		// if the module is sendsms user not send the verification code here
+		// the user send the verification code to my sms service
+		// and this code is deffirent by verification code
+		if($_module === 'sendsms')
+		{
+			$code = utility::post('code');
+			if($code == self::get_enter_session('sendsms_code'))
+			{
+				$log_id = self::get_enter_session('sendsms_code_log_id');
+
+				if($log_id)
+				{
+					$get_log_detail = \lib\db\logs::get(['id' => $log_id, 'limit' => 1]);
+					if(!$get_log_detail || !isset($get_log_detail['log_status']))
+					{
+						\lib\db\logs::set('enter:verify:sendsmsm:log:not:found', self::user_data('id'), $log_meta);
+						debug::error(T_("System error, try again"));
+						return false;
+					}
+
+					switch ($get_log_detail['log_status'])
+					{
+						case 'deliver':
+							// the user must be login
+							\lib\db\logs::update(['log_status' => 'expire'], $log_id);
+							$code_is_okay = true;
+							// set login session
+							// $redirect_url = self::enter_set_login();
+
+							// // save redirect url in session to get from okay page
+							// self::set_enter_session('redirect_url', $redirect_url);
+							// // set okay as next step
+							// self::next_step('okay');
+							// // go to okay page
+							// self::go_to('okay');
+							break;
+
+						case 'enable':
+							// user not send sms or not deliver to us
+							\lib\db\logs::set('enter:verify:sendsmsm:sms:not:deliver:to:us', self::user_data('id'), $log_meta);
+							debug::error(T_("Your sms not deliver to us!"));
+							return false;
+							break;
+
+						case 'expire':
+							// the user user from this way and can not use this way again
+							// this is a bug!
+							\lib\db\logs::set('enter:verify:sendsmsm:sms:expire:log:bug', self::user_data('id'), $log_meta);
+							debug::error(T_("What are you doing?"));
+							return false;
+						default:
+							// bug!
+							return false;
+							break;
+					}
+				}
+				else
+				{
+					\lib\db\logs::set('enter:verify:sendsmsm:log:id:not:found', self::user_data('id'), $log_meta);
+					debug::error(T_("What are you doing?"));
+					return false;
+				}
+			}
+			else
+			{
+				\lib\db\logs::set('enter:verify:sendsmsm:user:inspected:change:html', self::user_data('id'), $log_meta);
+				debug::error(T_("What are you doing?"));
+				return false;
+			}
+		}
+		else
+		{
+			if(intval(utility::post('code')) === intval(self::get_enter_session('verification_code')))
+			{
+				$code_is_okay = true;
+			}
+		}
+
+		if($code_is_okay)
 		{
 			// expire code
 			if(self::get_enter_session('verification_code_id'))
