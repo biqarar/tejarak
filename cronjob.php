e007034f2d7866d585ee7947158c9cf990f2e8be
Reza <rm.biqarar@gmail.com>
2017-09-09 12:20:29 +0430

    cronjob bug fix

--- cronjob.php
+++ cronjob.php
@@ -32,10 +32,10 @@ class cronjob
 	public function requests()
 	{
 		$requests   = [];
-		$requests[] = ['url' => 'https://tejarak.com/fa/cronjob/pinger', 'header' => []];
-		$requests[] = ['url' => 'https://tejarak.com/fa/cronjob/report', 'header' => []];
-		$requests[] = ['url' => 'https://tejarak.com/fa/cronjob/notification', 'header' => []];
-		$requests[] = ['url' => 'https://tejarak.com/fa/cronjob/calc', 'header' => []];
+		$requests[] = ['url' => 'https://tejarak.com/cronjob/pinger', 'header' => []];
+		$requests[] = ['url' => 'https://tejarak.com/cronjob/report', 'header' => []];
+		$requests[] = ['url' => 'https://tejarak.com/cronjob/notification', 'header' => []];
+		$requests[] = ['url' => 'https://tejarak.com/cronjob/calc', 'header' => []];
 
 		// $requests[] = ['url' => 'http://tejarak.dev/fa/cronjob/report', 'header' => []];
 		// $requests[] = ['url' => 'http://tejarak.dev/fa/cronjob/calc', 'header' => []];
