e324e250726036bd8ab437d28ec92d2bbac16247
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-09-22 17:07:56 +0330

    add life to body on attendance page

--- content/hours/view.php
+++ content/hours/view.php
@@ -9,6 +9,9 @@ class view extends \mvc\view
 	{
 		$this->data->bodyclass    = 'unselectable siftal attendance';
 		$this->data->current_time = date("Y-m-d H:i:s");
+
+		// add life to page to refresh automatically after end this time
+		$this->data->bodyel = 'data-life=20000';
 	}
 
 
