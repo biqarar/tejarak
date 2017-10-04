8ea5c0dc587b9317f5e7520dde57532261258253
Reza <rm.biqarar@gmail.com>
2017-10-04 11:10:12 +0330

    send sms fix

--- includes/option/sms.php
+++ includes/option/sms.php
@@ -2,14 +2,14 @@
 /**
 * sms kavenegar config
 */
-self::$sms['kavenegar']['status']             = true;
-self::$sms['kavenegar']['apikey']             = '783067644A597A41716F3734755A683152736F6673773D3D';
-self::$sms['kavenegar']['debug']              = null;
-self::$sms['kavenegar']['line']              = '10006660066600';
-self::$sms['kavenegar']['iran']               = null;
-self::$sms['kavenegar']['header']             = null;
-self::$sms['kavenegar']['footer']             = 'tejarak';
-self::$sms['kavenegar']['one']                = true;
+self::$sms['kavenegar']['status'] = true;
+self::$sms['kavenegar']['apikey'] = '783067644A597A41716F3734755A683152736F6673773D3D';
+self::$sms['kavenegar']['debug']  = null;
+self::$sms['kavenegar']['line']   = '10006660066600';
+self::$sms['kavenegar']['iran']   = true;
+self::$sms['kavenegar']['header'] = null;
+self::$sms['kavenegar']['footer'] = 'tejarak';
+self::$sms['kavenegar']['one']    = true;
 
 
 ?>
