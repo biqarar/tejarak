974dba3a2dcb1df4d6f652d00113007a9173d195
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-10-05 17:43:58 +0330

    add simple design to dashboard

--- content_a/dashboard/view.php
+++ content_a/dashboard/view.php
@@ -3,6 +3,19 @@ namespace content_a\dashboard;
 
 class view extends \content_a\main\view
 {
+	public function config()
+	{
 
+		parent::config();
+		$team_name = '';
+		if(isset($this->data->current_team['name']))
+		{
+			$team_name = $this->data->current_team['name'];
+		}
+		$this->data->page['title'] = T_('Dashboard of :name', ['name'=> $team_name]);
+		$this->data->page['desc'] = T_('Glance at your team summary and compare some important data together and enjoy Tejarak!'). ' '. T_('Have a good day;)');
+
+
+	}
 }
 ?>
