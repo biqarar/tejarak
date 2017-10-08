5405e401c9010e7017a00bce6756f911aba576fe
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2017-10-08 04:45:21 +0330

    fix add team

--- content_a/team/add/view.php
+++ content_a/team/add/view.php
@@ -9,7 +9,7 @@ class view extends \content_a\main\view
 	public function view_add()
 	{
 		$this->data->page['title'] = T_('Add new team');
-		$this->data->page['desc']  = $this->data->page['title'];
+		$this->data->page['desc']  = T_('Set some basic detail, after insert you can add avatar and change all settings');
 	}
 }
 ?>
