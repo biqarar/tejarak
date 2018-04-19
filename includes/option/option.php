c0b86f12399d3f41048fd7e259eef399287d029c
Reza <rm.biqarar@gmail.com>
2018-04-19 12:57:16 +0430

    fix tejarak tld

--- includes/option/option.php
+++ includes/option/option.php
@@ -27,37 +27,17 @@ self::$language =
  * system default lanuage
  */
 
-self::$config['redirect_url']                 = 'http://tejarak.local';
-self::$config['multi_domain']                 = true;
-self::$config['redirect_to_main']             = true;
-self::$config['https']                        = false;
-self::$config['default_tld']                  = 'local';
-self::$config['default_permission']           = null;
-self::$config['debug']                        = true;
-self::$config['coming']                       = false;
-self::$config['short_url']                    = null;
-self::$config['save_as_cookie']               = false;
-self::$config['log_visitors']                 = true;
-self::$config['passphrase']                   = null;
-self::$config['passkey']                      = null;
-self::$config['passvalue']                    = null;
-self::$config['default']                      = null;
-self::$config['redirect']                     = 'a';
-self::$config['register']                     = true;
-self::$config['recovery']                     = true;
-self::$config['fake_sub']                     = null;
-self::$config['real_sub']                     = true;
-self::$config['force_short_url']              = null;
-self::$config['sms']                          = true;
+// fix url and redirect
+// http://tejarak.ir
+// to https://tejarak.com
+self::$url['tld']                   = 'com';
+self::$url['protocol']              = 'https';
 
-self::$config['account']                      = true;
-self::$config['main_account']                 = null;
-self::$config['account_status']               = true;
-self::$config['use_main_account']             = false;
 
-self::$config['domain_same']                  = true;
-self::$config['domain_name']                  = 'tejarak';
-self::$config['main_site']                    = 'http://tejarak.local';
+
+self::$config['debug']              = false;
+self::$config['coming']             = false;
+self::$config['redirect']           = 'a';
 
 
 
