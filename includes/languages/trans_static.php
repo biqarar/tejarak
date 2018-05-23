4c7dbb07edfdec0dc8785597aa7e32a551fd3f62
Reza <rm.biqarar@gmail.com>
2018-05-23 18:07:11 +0430

    update static trans

---
+++ includes/languages/trans_static.php
@@ -0,0 +1,94 @@
+<?php
+class TransStatic
+{
+ private function trans_static()
+ {
+
+	echo T_("Saloos");
+	echo T_("2013");
+	echo T_("2014");
+	echo T_("2015");
+	echo T_("2016");
+	echo T_("2017");
+	echo T_("2018");
+	echo T_("2019");
+
+	echo T_("Sadegh Salehi");
+	echo T_("Ali Modabber");
+	echo T_("SeyedMahdi Hoseini");
+	echo T_("Ali Kharidar");
+	echo T_("MohamadAli Salehi");
+	echo T_("Sara Torkamani");
+	echo T_("Saeed Bodaghi");
+	echo T_("SeyedMohsen Mortazavi");
+	echo T_("SeyedMohsen Mortazavi Hoseini");
+	echo T_("Arash Shakery");
+	echo T_("Majid Hamidian");
+	echo T_("Moslem Hamzehnejadi");
+	echo T_("MohamadHosein Hamzehnejadi");
+	echo T_("Saman Soltani");
+	echo T_("Mohamad Aboutalebi");
+	echo T_("Mojtaba Sadeghi");
+	echo T_("Mahdi Karimi");
+
+	echo T_("hours in this week");
+	echo T_("hours in this month");
+	echo T_("hours remote in this month");
+	echo T_("hours in idle this month");
+
+	echo T_("Founder & CEO");
+	echo T_("Founder and CEO");
+	echo T_("CEO & Solution Designer");
+	echo T_("CEO and Solution Designer");
+	echo T_("CEO");
+	echo T_("Solution Designer");
+	echo T_("Full Stack Developer");
+	echo T_("Backend Developer");
+	echo T_("Frontend Designer");
+	echo T_("UI Designer");
+	echo T_("Graphic Designer");
+	echo T_("Shareholder");
+	echo T_("Chrikt");
+	echo T_("Content Expert");
+	echo T_("Content Expert");
+	echo T_("Service");
+	echo T_("UI Developer");
+	echo T_("Frontend Developer");
+	echo T_("Research & Develepment");
+	echo T_("Research &amp; Develepment");
+	echo T_("UI Expert");
+	echo T_("Android Developer");
+	echo T_("IOS Developer");
+	echo T_("WP Developer");
+	echo T_("Nothing!");
+	echo T_("Traveler");
+	echo T_("Intern");
+
+
+
+	echo T_("name");
+	echo T_("until");
+	echo T_("summary");
+	echo T_("ganje");
+	echo T_("Ganje");
+	echo T_("Centeral");
+	echo T_("centeral");
+	echo T_("Simple, transparent pricing");
+	echo T_("Pay by zarinpal payment");
+	echo T_("Pay by parsian payment");
+
+	echo T_("General");
+	echo T_("Board");
+
+
+
+	echo T_(" ");
+
+	echo T_("money");
+	echo T_("toman");
+	echo T_("Tejarak; Modern Approach");
+
+
+ }
+}
+?>
