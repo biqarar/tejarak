f65c6d69b6edd1187a9bf44a1d6fd9b8fc77eb3a
Reza <rm.biqarar@gmail.com>
2018-10-09 08:12:49 +0330

    fix log database

---
+++ includes/database/upgrade/(db_name)/v.1.7.2_db_log.sql
@@ -0,0 +1,84 @@
+
+CREATE TABLE `agents` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `agent` text NOT NULL,
+  `group` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `version` varchar(50) DEFAULT NULL,
+  `os` varchar(50) DEFAULT NULL,
+  `osnum` varchar(50) DEFAULT NULL,
+  `robot` bit(1) DEFAULT NULL,
+  `meta` text,
+  `datecreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `datemodified` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `agentmd5` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visitors`
+--
+
+CREATE TABLE `visitors` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `statuscode` int(5) DEFAULT NULL,
+  `visitor_ip` int(10) UNSIGNED DEFAULT NULL,
+  `session_id` varchar(100) DEFAULT NULL,
+  `url_id` int(10) UNSIGNED NOT NULL,
+  `url_idreferer` int(10) UNSIGNED DEFAULT NULL,
+  `agent_id` int(10) UNSIGNED DEFAULT NULL,
+  `user_id` int(10) UNSIGNED DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `avgtime` int(10) UNSIGNED DEFAULT NULL,
+  `country` char(2) DEFAULT NULL,
+  `method` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `agents`
+--
+ALTER TABLE `agents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `visitors`
+--
+ALTER TABLE `visitors`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `visitors_agents` (`agent_id`),
+  ADD KEY `visitors_urls` (`url_id`),
+  ADD KEY `visitors_urls_referer` (`url_idreferer`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `agents`
+--
+ALTER TABLE `agents`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `visitors`
+--
+ALTER TABLE `visitors`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+--
+-- Constraints for table `visitors`
+--
+ALTER TABLE `visitors`
+  ADD CONSTRAINT `visitors_agents` FOREIGN KEY (`agent_id`) REFERENCES `agents` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `visitors_urls` FOREIGN KEY (`url_id`) REFERENCES `urls` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `visitors_urls_referer` FOREIGN KEY (`url_idreferer`) REFERENCES `urls` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+COMMIT;
+
